682cf5ea73a756a07116b08a4dc436b3611087fd
BeatZ1337 <59895507+BeatZ1337@users.noreply.github.com>
2020-07-01 07:01:07 -0500

    Delete beaters2.sh

--- beaters2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget http://www.softether-download.com/files/softether/v4.32-9731-beta-2020.01.01-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.32-9731-beta-2020.01.01-linux-x64-64bit.tar.gz
-tar zxf softether-vpnserver-v4.32-9731-beta-2020.01.01-linux-x64-64bit.tar.gz
