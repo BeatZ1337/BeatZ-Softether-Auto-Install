685eab95ff0353acd04ef5a8faab3e8bb6590bb3
BeatZ1337 <59895507+BeatZ1337@users.noreply.github.com>
2020-07-01 04:45:27 -0500

    Add files via upload

---
+++ beaters.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+DAEMON=/usr/local/vpnserver/vpnserver
+LOCK=/var/lock/subsys/vpnserver
+test -x $DAEMON || exit 0
+case "$1" in
+start)
+$DAEMON start
+touch $LOCK
+;;
+stop)
+$DAEMON stop
+rm $LOCK
+;;
+restart)
+$DAEMON stop
+sleep 3
+$DAEMON start
+;;
+*)
+echo "Usage: $0 {start|stop|restart}"
+exit 1
+esac
+exit 0
